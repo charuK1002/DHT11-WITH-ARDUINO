262c47b324bf47c8a9303ba2a2abea7a6207a585
charuK1002 <charukhandelwal610@gmail.com>
2026-03-11 17:16:30 +0530

    docs: added doxygen documentation

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
 #include <Arduino.h>
+#include <DHT.h>
+#define DHTPIN 2     // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT11   // DHT 11
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-// write your initialization code here
+    // write your initialization code here
+    dht.begin();
+    Serial.println("DHT11 Sensor Initialized");
 }
 
 void loop() {
-// write your code here
+    // write your code here
+    float humidity = dht.readHumidity();
+    float temperature = dht.readTemperature();
+
+    if (isnan(humidity) || isnan(temperature)) {
+        Serial.println("Failed to read from DHT sensor!");
+        return;
+    }
+
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.print(" %\t");
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.println(" *C");
+
+    delay(2000); // Wait a few seconds between measurements
 }
